59fb5785c42cc86b6b1dd7f4bf8727b5312b5d52
Samuel Attwood <sam@attwood.io>
2023-03-01 22:49:33 -0500

    Updating validate to allow pass if only catalog.cattle.io annotations are modified

--- pkg/validate/validate.go
+++ pkg/validate/validate.go
@@ -11,7 +11,9 @@ import (
 
 	"github.com/go-git/go-git/v5"
 	"github.com/go-git/go-git/v5/plumbing"
+	"github.com/samuelattwood/partner-charts-ci/pkg/conform"
 	"github.com/sirupsen/logrus"
+	"helm.sh/helm/v3/pkg/chart/loader"
 
 	"sigs.k8s.io/yaml"
 )
@@ -128,7 +130,17 @@ func CompareDirectories(leftPath, rightPath string, exclude map[string]struct{})
 				logrus.Error(err)
 			}
 
-			if leftCheckSum != rightCheckSum {
+			if leftCheckSum != rightCheckSum && strings.HasSuffix(filePath, ".tgz") {
+				chartMatch, err := matchHelmCharts(filePath, rightFilePath)
+				if chartMatch {
+					directoryComparison.Unchanged = append(directoryComparison.Unchanged, relativePath)
+				} else {
+					directoryComparison.Modified = append(directoryComparison.Modified, relativePath)
+				}
+				if err != nil {
+					logrus.Debug(err)
+				}
+			} else if leftCheckSum != rightCheckSum {
 				directoryComparison.Modified = append(directoryComparison.Modified, relativePath)
 			} else {
 				directoryComparison.Unchanged = append(directoryComparison.Unchanged, relativePath)
@@ -159,4 +171,64 @@ func CompareDirectories(leftPath, rightPath string, exclude map[string]struct{})
 	}
 
 	return directoryComparison, nil
+}
+
+func matchHelmCharts(leftPath, rightPath string) (bool, error) {
+	leftFile, err := os.Open(leftPath)
+	if err != nil {
+		return false, err
+	}
+	defer leftFile.Close()
+
+	rightFile, err := os.Open(rightPath)
+	if err != nil {
+		return false, err
+	}
+	defer rightFile.Close()
+
+	leftChart, err := loader.LoadArchive(leftFile)
+	if err != nil {
+		return false, err
+	}
+
+	rightChart, err := loader.LoadArchive(rightFile)
+	if err != nil {
+		return false, err
+	}
+
+	for annotation := range leftChart.Metadata.Annotations {
+		if strings.HasPrefix(annotation, "catalog.cattle.io") {
+			delete(leftChart.Metadata.Annotations, annotation)
+		}
+	}
+
+	for annotation := range rightChart.Metadata.Annotations {
+		if strings.HasPrefix(annotation, "catalog.cattle.io") {
+			delete(rightChart.Metadata.Annotations, annotation)
+		}
+	}
+
+	tempDir, err := os.MkdirTemp(os.TempDir(), "chartValidate")
+	if err != nil {
+		return false, err
+	}
+	defer os.RemoveAll(tempDir)
+
+	leftOut := path.Join(tempDir, "left")
+	rightOut := path.Join(tempDir, "right")
+
+	err = conform.ExportChartDirectory(leftChart, leftOut)
+	if err != nil {
+		return false, err
+	}
+
+	err = conform.ExportChartDirectory(rightChart, rightOut)
+	if err != nil {
+		return false, err
+	}
+
+	directoryComparison, err := CompareDirectories(leftOut, rightOut, map[string]struct{}{})
+
+	return directoryComparison.Match, err
+
 }
